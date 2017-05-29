cc69939653d7bee7ce424c3507390b3cea4e5e70
Bharat Poptwani <b4bharat.available@gmail.com>
2017-05-29 21:37:12 +0530

    added 'css' and 'style' loaders to the webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,34 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  'template': './client/index.html',
-  'filename': 'index.html',
-  'inject': 'body'
+  template: './client/index.html',
+  filename: 'index.html',
+  inject: 'body'
 });
 
 module.exports = {
-  'entry': './client/index.js',
-  'output': {
-    'path': path.resolve('dist'),
-    'filename': 'index_bundle.js'
+  entry: './client/index.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'index_bundle.js'
   },
-  'module': {
-    'loaders': [
+  module: {
+    loaders: [
       {
-        'test': /\.js$/,
-        'loader': 'babel-loader',
-        'exclude': '/node_modules/'
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: '/node_modules/'
       },
       {
-        'test': /\.jsx$/,
-        'loader': 'babel-loader',
-        'exclude': '/node_modules/'
+        test: /\.jsx$/,
+        loader: 'babel-loader',
+        exclude: '/node_modules/'
+      },
+      {
+        test: /\.css$/,
+        loaders: ['style-loader', 'css-loader']
       }
     ]
   },
-  'plugins': [HtmlWebpackPluginConfig]
+  plugins: [HtmlWebpackPluginConfig]
 };
