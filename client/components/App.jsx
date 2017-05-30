98a5925de99d4f5344acec736fdf4ed9c0361ab1
Bharat Poptwani <b4bharat.available@gmail.com>
2017-05-30 07:40:49 +0530

    basic styling added to the page

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default class App extends React.Component {
   render() {
     return (
-      <div style={{'textAlign': 'center'}}>
+      <div className='container'>
         <h1>Hello World</h1>
       </div>
     );
