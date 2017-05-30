e620e59dc28cb72eadab4151a0c89fc8111d2cdf
Bharat Poptwani <b4bharat.available@gmail.com>
2017-05-30 08:52:39 +0530

    minor styling and markup changes

--- client/components/Landing.jsx
+++ client/components/Landing.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
-export default class Landing extends React.Component {
-  render() {
-    return (
-      <div>
-        <div className='title-image'>
-          <img src={require('../images/modus_green.png')} alt="title image"/>
-        </div>
+export default function Landing(props) {
+  return (
+    <div>
+      <div className='title-image'>
+        <img src={require('../images/modus_green.png')} alt="title image"/>
       </div>
-    );
-  }
+      <div className='intro-text'>
+        <h1>Welcome Aboard!</h1>
+        <p>A one of a kind platform to discover, create, manage blogs</p>
+      </div>
+    </div>
+  );
 }
