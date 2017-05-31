4cf4535ef504d9673da50bfa9a8b3e96a113d67d
Bharat Poptwani <b4bharat.available@gmail.com>
2017-05-31 10:30:52 +0530

    completed the task

--- client/components/FeedItemDetails.jsx
+++ client/components/FeedItemDetails.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import articleAPI from '../utils/articleAPI.js';
-import { HeaderNavigation } from './FeedItemElements.jsx';
+import { HeaderNavigation, PublishDetails } from './FeedItemElements.jsx';
 
 export default function FeedItemDetails(props) {
   const article = articleAPI.get(
     parseInt(props.match.params.articleId, 10)
   );
-  
+
+  const coverPhotoStyle = {
+    backgroundImage: `url(http://lorempixel.com/375/350/${article.imgCategory}/)`
+  };
+
   return (
     <div className='container feed-item-detail'>
       <HeaderNavigation articleId={props.match.params.articleId}></HeaderNavigation>
-      <div className='cover-photo'>
-        <img src="http://lorempixel.com/375/300/fashion/" alt="fashion 1"/>
+      <div className='cover-photo' style={coverPhotoStyle}>
+        <div className='article-category'>{article.category}</div>
+        <h1 className='article-title'>{article.title}</h1>
+        <PublishDetails
+          publishDate={article.publishDate}
+          lengthInMins={article.lengthInMins}
+          readerCount={article.readerCount}
+        ></PublishDetails>
       </div>
       <div className='feed-article'>
         {article.articleText}
